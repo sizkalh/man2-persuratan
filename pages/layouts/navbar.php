00095c050746556d0f814cbed8842e103e6c10dd
seftianandy <seftianandy45@gmail.com>
2022-10-27 03:04:11 +0700

    update notifikasi
    update waktu notifikasi

--- pages/layouts/navbar.php
+++ pages/layouts/navbar.php
@@ -39,30 +39,32 @@
                                                       WHERE tbl_guru.pangkat = "' .  $_SESSION['pangkat_user'] . '" 
                                                       AND tbl_guru.id = "' .  $_SESSION['id_user'] . '"
                                                       AND tbl_tanda_tangan.status = "cek"
-                                                      ');
-                $query_notif = mysqli_query($koneksi, 'SELECT 
-                                                      *
-                                                      FROM tbl_tanda_tangan 
-                                                      INNER JOIN tbl_guru ON tbl_guru.id=tbl_tanda_tangan.id_user 
-                                                      INNER JOIN tbl_surat ON tbl_surat.id=tbl_tanda_tangan.id_surat 
-                                                      WHERE tbl_guru.pangkat = "' .  $_SESSION['pangkat_user'] . '" 
+                                                      AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                      OR tbl_guru.pangkat = "' .  $_SESSION['pangkat_user'] . '" 
                                                       AND tbl_guru.id = "' .  $_SESSION['id_user'] . '"
                                                       AND tbl_tanda_tangan.status = "cek"
+                                                      AND tbl_tanda_tangan.status_notif = "diajukan"
                                                       ');
+                // $query_notif = mysqli_query($koneksi, 'SELECT 
+                //                                       *
+                //                                       FROM tbl_tanda_tangan 
+                //                                       INNER JOIN tbl_guru ON tbl_guru.id=tbl_tanda_tangan.id_user 
+                //                                       INNER JOIN tbl_surat ON tbl_surat.id=tbl_tanda_tangan.id_surat 
+                //                                       WHERE tbl_guru.pangkat = "' .  $_SESSION['pangkat_user'] . '" 
+                //                                       AND tbl_guru.id = "' .  $_SESSION['id_user'] . '"
+                //                                       AND tbl_tanda_tangan.status = "cek"
+                //                                       AND tbl_tanda_tangan.status_notif = "ditolak"
+                //                                       ');
                 $count = mysqli_fetch_array($query_count);
-                $notif = mysqli_fetch_array($query_notif);
+                // $notif = mysqli_fetch_array($query_notif);
 
                 if ($count['notif'] != 0) {
-                  if ($notif['status_notif'] != "belum") {
                 ?>
-                    <i class="fa fa-bell animate__animated animate__swing animate__infinite"></i> Notifikasi
-                    <span class="label label-danger">
-                      <?= $count['notif']; ?>
-                    </span>
-                  <?php } else { ?>
-                    <i class="fa fa-bell"></i> Notifikasi
-                  <?php }
-                } else { ?>
+                  <i class="fa fa-bell animate__animated animate__swing animate__infinite"></i> Notifikasi
+                  <span class="label label-danger">
+                    <?= $count['notif']; ?>
+                  </span>
+                <?php } else { ?>
                   <i class="fa fa-bell"></i> Notifikasi
                 <?php } ?>
 
@@ -105,12 +107,12 @@
                                                                 ON tbl_guru.id = tbl_tanda_tangan.id_user
                                                               INNER JOIN tbl_surat
                                                                 ON tbl_surat.id = tbl_tanda_tangan.id_surat
-                                                            WHERE tbl_tanda_tangan.status = "diterima"
-                                                            AND tbl_tanda_tangan.status_notif = "diterima"
-                                                            AND tbl_guru.pangkat = "katu"
-                                                            OR tbl_tanda_tangan.status = "ditolak"
-                                                            AND tbl_tanda_tangan.status_notif = "ditolak"
-                                                            AND tbl_guru.pangkat = "superuser"
+                                                            WHERE tbl_tanda_tangan.status = "cek"
+                                                              AND tbl_tanda_tangan.status_notif = "diajukan"
+                                                              AND tbl_guru.pangkat = "kamad"
+                                                              OR tbl_tanda_tangan.status = "cek"
+                                                              AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                              AND tbl_guru.pangkat = "kamad"
                                                             GROUP BY tbl_tanda_tangan.id_surat
                                                             ORDER BY tbl_tanda_tangan.tgl_proses DESC
                                                               ');
@@ -127,12 +129,12 @@
                                                                 ON tbl_guru.id = tbl_tanda_tangan.id_user
                                                               INNER JOIN tbl_surat
                                                                 ON tbl_surat.id = tbl_tanda_tangan.id_surat
-                                                            WHERE tbl_tanda_tangan.status = "diterima"
-                                                            AND tbl_tanda_tangan.status_notif = "diterima"
-                                                            AND tbl_guru.pangkat = "operator"
-                                                            OR tbl_tanda_tangan.status = "ditolak"
-                                                            AND tbl_tanda_tangan.status_notif = "ditolak"
-                                                            AND tbl_guru.pangkat = "kamad"
+                                                            WHERE tbl_tanda_tangan.status = "cek"
+                                                              AND tbl_tanda_tangan.status_notif = "diajukan"
+                                                              AND tbl_guru.pangkat = "katu"
+                                                              OR tbl_tanda_tangan.status = "cek"
+                                                              AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                              AND tbl_guru.pangkat = "katu"
                                                             GROUP BY tbl_tanda_tangan.id_surat
                                                             ORDER BY tbl_tanda_tangan.tgl_proses DESC
                                                               ');
@@ -149,12 +151,12 @@
                                                                 ON tbl_guru.id = tbl_tanda_tangan.id_user
                                                               INNER JOIN tbl_surat
                                                                 ON tbl_surat.id = tbl_tanda_tangan.id_surat
-                                                            WHERE tbl_tanda_tangan.status = "diterima"
-                                                            AND tbl_tanda_tangan.status_notif = "diterima"
-                                                            AND tbl_guru.pangkat = "guru"
-                                                            OR tbl_tanda_tangan.status = "ditolak"
-                                                            AND tbl_tanda_tangan.status_notif = "ditolak"
-                                                            AND tbl_guru.pangkat = "katu"
+                                                            WHERE tbl_tanda_tangan.status = "cek"
+                                                              AND tbl_tanda_tangan.status_notif = "diajukan"
+                                                              AND tbl_guru.pangkat = "operator"
+                                                              OR tbl_tanda_tangan.status = "cek"
+                                                              AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                              AND tbl_guru.pangkat = "operator"
                                                             GROUP BY tbl_tanda_tangan.id_surat
                                                             ORDER BY tbl_tanda_tangan.tgl_proses DESC
                                                               ');
@@ -171,14 +173,15 @@
                                                                 ON tbl_guru.id = tbl_tanda_tangan.id_user
                                                               INNER JOIN tbl_surat
                                                                 ON tbl_surat.id = tbl_tanda_tangan.id_surat
-                                                            WHERE tbl_tanda_tangan.status = "ditolak"
+                                                            WHERE tbl_tanda_tangan.status = "cek"
                                                             AND tbl_tanda_tangan.status_notif = "ditolak"
-                                                            AND tbl_guru.pangkat = "operator"
+                                                            AND tbl_guru.pangkat = "guru"
+                                                            AND tbl_guru.id = "' .  $_SESSION['id_user'] . '"
                                                             GROUP BY tbl_tanda_tangan.id_surat
                                                             ORDER BY tbl_tanda_tangan.tgl_proses DESC
                                                               ');
                     }
-                    
+
 
                     while ($data = mysqli_fetch_array($query_notif)) {
                       if ($data['status_notif'] != "belum") {
@@ -251,20 +254,69 @@
                           </div>
                           <h4>
                             <?php
-                            $query_pemohon = mysqli_query($koneksi, 'SELECT tbl_guru.nama FROM tbl_guru INNER JOIN tbl_surat ON tbl_surat.id_pemohon = tbl_guru.id WHERE tbl_surat.id = "' . $data['id_surat'] . '"');
+                            $query_pemohon = mysqli_query($koneksi, 'SELECT tbl_guru.nama 
+                                                                      FROM tbl_guru 
+                                                                      INNER JOIN tbl_surat 
+                                                                      ON tbl_surat.id_pemohon = tbl_guru.id 
+                                                                      WHERE tbl_surat.id = "' . $data['id_surat'] . '"
+                                                                      ');
                             while ($pemohon = mysqli_fetch_array($query_pemohon)) {
                             ?>
                               <?= $pemohon['nama'] ?>
                             <?php } ?>
                             <?php
                             if ($_SESSION['pangkat_user'] == 'operator') {
-                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT tgl_proses FROM tbl_tanda_tangan INNER JOIN tbl_guru ON tbl_guru.id=tbl_tanda_tangan.id_user WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '" AND tbl_guru.pangkat = "guru"');
+                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT
+                                                                            tgl_proses
+                                                                          FROM
+                                                                            tbl_tanda_tangan
+                                                                            INNER JOIN tbl_guru
+                                                                              ON tbl_guru.id = tbl_tanda_tangan.id_user
+                                                                          WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "guru"
+                                                                            AND tbl_tanda_tangan.status_notif = "diterima"
+                                                                            OR tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "katu"
+                                                                            AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                                          ');
                             } else if ($_SESSION['pangkat_user'] == 'katu') {
-                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT tgl_proses FROM tbl_tanda_tangan INNER JOIN tbl_guru ON tbl_guru.id=tbl_tanda_tangan.id_user WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '" AND tbl_guru.pangkat = "operator"');
+                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT
+                                                                            tgl_proses
+                                                                          FROM
+                                                                            tbl_tanda_tangan
+                                                                            INNER JOIN tbl_guru
+                                                                              ON tbl_guru.id = tbl_tanda_tangan.id_user
+                                                                          WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "operator"
+                                                                            AND tbl_tanda_tangan.status_notif = "diterima"
+                                                                            OR tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "kamad"
+                                                                            AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                                          ');
                             } else if ($_SESSION['pangkat_user'] == 'kamad') {
-                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT tgl_proses FROM tbl_tanda_tangan INNER JOIN tbl_guru ON tbl_guru.id=tbl_tanda_tangan.id_user WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '" AND tbl_guru.pangkat = "katu"');
-                            } else if ($_SESSION['pangkat_user'] == 'guru' || $_SESSION['pangkat_user'] == 'superuser') {
-                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT tgl_proses FROM tbl_tanda_tangan INNER JOIN tbl_guru ON tbl_guru.id=tbl_tanda_tangan.id_user WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '" AND tbl_guru.pangkat = "kamad"');
+                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT
+                                                                            tgl_proses
+                                                                          FROM
+                                                                            tbl_tanda_tangan
+                                                                            INNER JOIN tbl_guru
+                                                                              ON tbl_guru.id = tbl_tanda_tangan.id_user
+                                                                          WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "katu"
+                                                                            AND tbl_tanda_tangan.status_notif = "diterima"
+                                                                            OR tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "superuser"
+                                                                            AND tbl_tanda_tangan.status_notif = "ditolak"
+                                                                          ');
+                            } else if ($_SESSION['pangkat_user'] == 'guru') {
+                              $query_tgl_notif = mysqli_query($koneksi, 'SELECT
+                                                                            tgl_proses
+                                                                          FROM
+                                                                            tbl_tanda_tangan
+                                                                            INNER JOIN tbl_guru
+                                                                              ON tbl_guru.id = tbl_tanda_tangan.id_user
+                                                                          WHERE tbl_tanda_tangan.id_surat = "' . $data['id_surat'] . '"
+                                                                            AND tbl_guru.pangkat = "operator"
+                                                                            AND tbl_tanda_tangan.status_notif = "ditolak"');
                             }
                             while ($tgl = mysqli_fetch_array($query_tgl_notif)) {
                             ?>
@@ -328,13 +380,13 @@
                             ?>
                           </p>
                           <p>
-                            <span <?php 
-                              if ($data['status_notif'] == "diterima"){
-                                echo 'class="text-info"';
-                              } else if($data['status_notif'] == "ditolak") {
-                                echo 'class="text-danger"';
-                              }
-                            ?>>
+                            <span <?php
+                                  if ($data['status_notif'] == "diterima") {
+                                    echo 'class="text-info"';
+                                  } else if ($data['status_notif'] == "ditolak") {
+                                    echo 'class="text-danger"';
+                                  }
+                                  ?>>
                               (<?= $data['status_notif'] ?>)
                             </span>
                           </p>
--- process/nota-dinas/tolakTandaTangan.php
+++ process/nota-dinas/tolakTandaTangan.php
@@ -14,31 +14,31 @@ $catatan = $_POST['catatan'];
 $id_user = $_SESSION['id_user'];
 $tgl_proses = date('Y-m-d H:i:s');
 
-$query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="ditolak", status_notif="belum", catatan = "' . $catatan . '", tgl_proses = "' . $tgl_proses . '" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user . '"');
+$query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="ditolak", status_notif="ditolak", catatan = "' . $catatan . '", tgl_proses = "' . $tgl_proses . '" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user . '"');
 
 if ($pangkat == "operator") {
     $cek_pangkat = mysqli_query($koneksi, 'SELECT tbl_guru.pangkat, tbl_tanda_tangan.* FROM tbl_guru INNER JOIN tbl_tanda_tangan ON tbl_tanda_tangan.id_user=tbl_guru.id WHERE tbl_guru.pangkat = "guru"');
     while ($data_pangkat = mysqli_fetch_array($cek_pangkat)) {
         $id_user_p = $data_pangkat['id_user'];
-        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="diajukan" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
+        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="ditolak" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
     }
 } else if ($pangkat == "katu") {
     $cek_pangkat = mysqli_query($koneksi, 'SELECT tbl_guru.pangkat, tbl_tanda_tangan.* FROM tbl_guru INNER JOIN tbl_tanda_tangan ON tbl_tanda_tangan.id_user=tbl_guru.id WHERE tbl_guru.pangkat = "operator"');
     while ($data_pangkat = mysqli_fetch_array($cek_pangkat)) {
         $id_user_p = $data_pangkat['id_user'];
-        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="diajukan" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
+        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="ditolak" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
     }
 } else if ($pangkat == "kamad") {
     $cek_pangkat = mysqli_query($koneksi, 'SELECT tbl_guru.pangkat, tbl_tanda_tangan.* FROM tbl_guru INNER JOIN tbl_tanda_tangan ON tbl_tanda_tangan.id_user=tbl_guru.id WHERE tbl_guru.pangkat = "katu"');
     while ($data_pangkat = mysqli_fetch_array($cek_pangkat)) {
         $id_user_p = $data_pangkat['id_user'];
-        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="diajukan" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
+        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="ditolak" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
     }
 } else if ($pangkat == "superuser") {
     $cek_pangkat = mysqli_query($koneksi, 'SELECT tbl_guru.pangkat, tbl_tanda_tangan.* FROM tbl_guru INNER JOIN tbl_tanda_tangan ON tbl_tanda_tangan.id_user=tbl_guru.id WHERE tbl_guru.pangkat = "kamad"');
     while ($data_pangkat = mysqli_fetch_array($cek_pangkat)) {
         $id_user_p = $data_pangkat['id_user'];
-        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="diajukan" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
+        $query_ttd = mysqli_query($koneksi, 'UPDATE tbl_tanda_tangan SET STATUS="cek", status_notif="ditolak" WHERE id_surat="' . $id_surat . '" AND id_user ="' . $id_user_p . '"');
     }
 }
 
